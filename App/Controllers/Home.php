bf0a5814b7d89023d85ccd5b49c16f8a5f61bb35
Paweł Bochacik <pawel.bochacik.programista@gmail.com>
2023-08-04 00:17:57 +0200

    Copied new MVC framework files to main project directory.

---
+++ App/Controllers/Home.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+/**
+ * Home controller
+ * 
+ * PHP version 7.4
+ */
+class Home extends \Core\Controller {
+    /**
+     * Before filter
+     * 
+     * @return void
+     */
+    protected function before() {
+        //echo "(before) ";
+        //return false;
+    }
+
+    /**
+     * After filter
+     * 
+     * @return void
+     */
+    protected function after() {
+        //echo " (after)";
+    }
+
+    /**
+     * How the index page
+     * 
+     * @return void
+     */
+    public function indexAction() {
+        /*
+        View::render('Home/index.php', [
+            'name'      => 'Dave',
+            'colours'   => ['red', 'green', 'blue']
+        ]);
+        */
+        View::renderTemplate('Home/index.html', [
+            'name'      => 'Dave',
+            'colours'   => ['red', 'green', 'blue']
+        ]);
+    }
+}
