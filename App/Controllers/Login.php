048d4d0b678a1b34bf8a62afffe669a7c1d83d66
Paweł Bochacik <pawel.bochacik.programista@gmail.com>
2023-09-30 00:48:43 +0200

    Added a new method to display flash message when user logging out

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -59,6 +59,18 @@ use \App\Flash;
     {
         Auth::logout();
 
-        $this->redirect('/');          
+        $this->redirect('/login/show-logout-message');          
     }
+
+    /**
+     * Show a 'logged out' flash message and redirect to the homepage. Necessary to use the flash messages as they use the session and at the end of the logout method (destroyAction) the session is destroyed so a new action needs to be called in order to use session.
+     * 
+     * @return void
+     */
+    public function showLogoutMessageAction() {
+        Flash::addMessage('Logout successful');
+        
+        $this->redirect('/');
+    }
+
  }
