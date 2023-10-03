13c06c2bba25899b23b93aeee628b544b7058aff
Pawel Bochacik <pawel.bochacik.programista@gmail.com>
2023-10-03 21:58:22 +0200

    Remembered login in a cookie

--- App/Auth.php
+++ App/Auth.php
@@ -25,9 +25,11 @@ class Auth
         $_SESSION['user_id'] = $user->id;
 
         if ($remember_me) {
-
-            $user->rememberLogin();
             
+            if ($user->rememberLogin()) {
+
+                setcookie('remember_me', $user->remember_token, $user->expiry_timestamp, '/');
+            }
         }
     }
 
