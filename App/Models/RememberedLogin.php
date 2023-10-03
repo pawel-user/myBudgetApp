61875ef0737d11f5fae37af4cdb4d6041752aef3
Pawel Bochacik <pawel.bochacik.programista@gmail.com>
2023-10-04 00:52:46 +0200

    Logged in automatically using token in cookie

---
+++ App/Models/RememberedLogin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+use \App\Models\User;
+use \App\Token;
+
+/**
+ * Remembered login model
+ * 
+ * PHP version 7.4
+ */
+#[\AllowDynamicProperties]
+class RememberedLogin extends \Core\Model {
+    /**
+     * Find a remembered login model by the token
+     * 
+     * @param string $token The remembered login token
+     * 
+     * @return mixed Remembered login object if found, false otherwise
+     */
+    public static function findByToken($token) {
+        $token = new Token($token);
+        $token_hash = $token->getHash();
+
+        $sql = 'SELECT * FROM remembered_logins
+                WHERE token_hash = :token_hash';
+        
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':token_hash', $token_hash, PDO::PARAM_STR);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
+
+    /**
+     * Get the user model associated with this remembered login
+     * 
+     * @return User The user model
+     */
+    public function getUser() {
+        return User::findByID($this->user_id);
+    }
+}
