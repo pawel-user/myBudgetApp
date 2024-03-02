8966e785e670671d95314612a8e5ed059b922db4
Paweł Bochacik <pawel.bochacik.programista@gmail.com>
2024-03-02 11:50:33 +0100

    Refactored html code on files summary.html and base.html

--- App/Models/User.php
+++ App/Models/User.php
@@ -289,7 +289,7 @@ class User extends \Core\Model
 
         $text = View::getTemplate('Password/reset_email.txt', ['url' => $url]);
         $html = View::getTemplate('Password/reset_email.html', ['url' => $url]);
-        
+
         Mail::send($this->email, 'Password reset', $text, $html);
     }
     
